b12a1bcdf271d2bc878d266690a4cc56a0458923
SANJAY G <56902792+Arthur-Nitzz@users.noreply.github.com>
2025-09-26 20:50:51 +0530

    Add files via upload

---
+++ src/pages/wishes/Wishes.tsx
@@ -0,0 +1,174 @@
+import "./Wishes.css";
+
+import { useNavigate, useParams } from "react-router-dom";
+
+import { motion } from "framer-motion";
+import { usePalette } from "@roylee1997/react-palette";
+
+import Progress from "../../components/Progress/Progress";
+import MusicCard from "../../components/MusicCard/MusicCard";
+import TMessagesData from "../../typings/MessagesData";
+
+// albumArts
+import firstAlbumArt from "../../assets/sampleData/first-album-art.webp";
+import secondAlbumArt from "../../assets/sampleData/second-album-art.webp";
+
+// musicFilePaths
+import firstMusic from "../../assets/sampleData/music/night-city.mp3";
+import secondMusic from "../../assets/sampleData/music/almost-nothing.mp3";
+
+// framer transition and variants
+const commonTransition = {
+  ease: [0.43, 0.13, 0.23, 0.96],
+  duration: 0.5,
+};
+
+const messageContainer = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.3,
+      ease: [0.43, 0.13, 0.23, 0.96],
+      duration: 0.5,
+    },
+  },
+};
+
+const messages = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+/* Each message must have music details (can be fetched through an API) with Album Art to be must) and message itself in multiple p tags (if possible) */
+// Sample Data
+const sampleMessagesDataArray: TMessagesData[] = [
+  {
+    albumArt: firstAlbumArt,
+    musicName: "Night City - R E L's Version",
+    messageInParas: [
+      "Hey Sneha! 🎉 Hope your birthday’s as amazing and fun as you are.",
+      "Wishing you a year full of laughter, unexpected adventures, and all the little things that make you smile.",
+      "May today be packed with cake, good vibes, and moments that make you feel like the star you are.",
+      "Here’s to another year of crushing it, enjoying life, and being unapologetically awesome. Happy Birthday, Sneha!",
+      "Swipe ---> Next Page!",
+    ],
+    musicFilePath: firstMusic,
+  },
+  {
+    albumArt: secondAlbumArt,
+    musicName: 'Almost nothing - "Death Stranding" Ending Song',
+    messageInParas: [
+      "Happy Birthday, Sneha! 🎉🎂 I hope your day is so amazing that even your cake is jealous of how sweet you are.",
+      "May your presents be better than your expectations, your selfies go viral and your WiFi never crash while uploading birthday Stories.",
+      "Eat cake like it’s a sport, laugh like everyone’s watching and dance like no one’s judging (even if your pet is)",
+      "Here’s to another year of being ridiculously awesome, slightly chaotic and totally unforgettable. Happy Birthday, Sneha!",
+      "Warm regards,",
+      "Arthur",
+    ],
+    musicFilePath: secondMusic,
+  },
+];
+
+const Wishes = () => {
+  const navigate = useNavigate();
+  const { id = 0 } = useParams();
+
+  if (Number(id) < 0 || sampleMessagesDataArray[Number(id)] == undefined) {
+    return <p>Invalid Wish Message Id</p>;
+  }
+
+  const {
+    data: colorData,
+    loading: colorDataIsLoading,
+    error,
+  } = usePalette(sampleMessagesDataArray[Number(id)].albumArt);
+
+  if (error) {
+    return <h1>Invalid Wish Message Id</h1>;
+  }
+
+  return (
+    <motion.main
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="wishes-wrapper h-screen w-screen flex flex-col justify-between items-center"
+    >
+      <Progress
+        primaryColor={colorData?.vibrant}
+        secondaryColor={colorData?.darkVibrant}
+        messageDataArrayLength={sampleMessagesDataArray.length}
+      />
+      <motion.div
+        className="lg:w-11/12 rounded-t-2xl md:rounded-t-xl flex md:flex-row flex-col-reverse"
+        style={{
+          backgroundColor: colorDataIsLoading ? "#fff" : colorData?.vibrant,
+        }}
+        initial={{ y: "1000px" }}
+        animate={{ y: "0px" }}
+        exit={{ y: "1000px" }}
+        transition={commonTransition}
+        drag="x"
+        dragConstraints={{ left: 0, right: 0 }}
+        dragElastic={0.1}
+        onDragEnd={(_, info) => {
+          if (info.offset.x > 50) {
+            if (Number(id) > 0 && Number(id) < sampleMessagesDataArray.length) {
+              navigate(`/wishes/${Number(id) - 1}`);
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }
+          } else if (info.offset.x < -50) {
+            if (
+              Number(id) >= 0 &&
+              Number(id) < sampleMessagesDataArray.length - 1
+            ) {
+              navigate(`/wishes/${Number(id) + 1}`);
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }
+          } else {
+            console.log(null);
+          }
+        }}
+      >
+        <motion.div
+          className="md:w-1/2"
+          initial="hidden"
+          animate="show"
+          variants={messageContainer}
+        >
+          {sampleMessagesDataArray[Number(id)].messageInParas.map(
+            (eachPara, index) => {
+              return (
+                <motion.p
+                  className="p-8 message text-3xl"
+                  variants={messages}
+                  key={index + 2045}
+                >
+                  {eachPara}
+                </motion.p>
+              );
+            }
+          )}
+        </motion.div>
+        <div className="md:w-1/2 h-1/2 md:h-auto flex justify-center items-center">
+          <MusicCard
+            albumArt={sampleMessagesDataArray[Number(id)].albumArt}
+            primaryColor={colorData?.vibrant}
+            musicName={sampleMessagesDataArray[Number(id)].musicName}
+            musicFilePath={sampleMessagesDataArray[Number(id)].musicFilePath}
+          />
+        </div>
+      </motion.div>
+    </motion.main>
+  );
+};
+
+export default Wishes;
